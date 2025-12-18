8b5a76db3a4b12f212b81a023d630e82e5750430
Dewin Salgado <dewinsnk@gmail.com>
2025-12-18 16:45:58 -0500

    Correcion de arranque del servidor

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
-// Importacion de express
-const express = required('express');
+// Importamos Express
+const express = require('express');
+
+// Inicializamos la aplicación
 const app = express();
 
 // Middleware para leer JSON
 app.use(express.json());
 
-// Uso de rutas
-app.use('/api/auth', authRoutes);
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.json({
+    mensaje: 'API de autenticación funcionando correctamente'
+  });
+});
 
 // Puerto del servidor
 const PORT = 3000;
 
-// Inicio del servidor
+// Levantamos el servidor
 app.listen(PORT, () => {
-    console.log(`Servidor ejecutandose en http://localhost:${PORT}`);
+  console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
